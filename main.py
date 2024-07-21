1023c8734403d7cd3292ad2d9d7dca0a6f82b619
victor <victor.ryzhikov@gmail.com>
2024-07-22 00:59:46 +0100

    Add iframe cors

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    path_regex=r"^/iframe/.*$"
+)
+
 
 @app.get("/calculator")
 async def get_calculators():
