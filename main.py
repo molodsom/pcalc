d515e2f4040732f1b3e00ea5b25b25baad052bfe
victor <victor.ryzhikov@gmail.com>
2024-07-26 03:50:26 +0100

    Add allow_origin_regex

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origin_regex=r"^/iframe/.*$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    path_regex=r"^/iframe/.*$"
 )
 
 
